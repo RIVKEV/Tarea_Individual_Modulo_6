0ac81c2698eb4188f5f85287086b2dbcccb5dad4
RIVKEV <117798759+RIVKEV@users.noreply.github.com>
2022-11-11 18:54:32 -0800

    Add files via upload

---
+++ Arreglos.java
@@ -0,0 +1,27 @@
+package arreglos;
+
+public class Arreglos {
+
+    
+    public static void main(String[] args) {
+    
+        String nombres[] = new String[10];
+        
+       nombres[0] = "David Hernandez ";
+       nombres[1] = "Marco Nuñez ";
+       nombres[2] = "Luis Vallecio ";
+       nombres[3] = "Marlon Carias ";
+       nombres[4] = "Tiffany Batiz ";
+       nombres[5] = "Victor Hernandez ";
+       nombres[6] = "Oscar Hernandez ";
+       nombres[7] = "Maynor Coello ";
+       nombres[8] = "Judy Castro ";
+       nombres[9] = "Delmer Mendoza ";
+       
+       for (int i=0 ; i<10; i++){
+           System.out.println(nombres[i]);
+       }
+           
+    }
+    
+}
