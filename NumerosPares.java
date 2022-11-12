1397e6e1d13515ce1a7c2d38eaf9317e4a14d7e4
RIVKEV <117798759+RIVKEV@users.noreply.github.com>
2022-11-11 18:55:46 -0800

    Add files via upload

---
+++ NumerosPares.java
@@ -0,0 +1,20 @@
+package numeros.pares;
+
+public class NumerosPares {
+
+    public static void main(String[] args) {
+        
+        int x=2;
+		
+
+			for(int i = 2; i<=50 ; i++ )
+					
+			{
+				System.out.println(x);
+				x=x+2;
+				
+			}
+			System.out.print(x);
+    }
+    
+}
