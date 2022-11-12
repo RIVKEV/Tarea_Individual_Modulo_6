07ced91a6bd199d91f43a5409588ab047311d1f4
RIVKEV <117798759+RIVKEV@users.noreply.github.com>
2022-11-11 18:59:33 -0800

    Add files via upload

---
+++ Validaciones.java
@@ -0,0 +1,47 @@
+package validaciones;
+
+public class Validaciones {
+
+  
+    public static void main(String[] args) {
+        
+        int M =6;
+	int T =1;
+	int K=-10;
+		
+	if (M>T)
+	{
+	System.out.println(true);
+	
+        }
+	else 
+	{ 
+	System.out.println(false);
+	
+        }
+	if (M/K==-5)
+	{
+	System.out.println(true);
+	
+        }
+	
+        else 
+	{
+	System.out.println(false);
+	
+        }
+
+	if (M+T==7||M-T==5)
+	{
+	System.out.println(true);
+		}
+        
+	else
+	{
+	System.out.println(false);
+	
+              }
+	}
+    
+    
+}
